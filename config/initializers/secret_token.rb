8268412fc963e1381edaaeee5a742860abcded88
Scott Albertson <scott.onix@gmail.com>
2011-04-21 18:49:28 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craigsbliss::Application.config.secret_token = '595cfe1479254dba724324cf9f043752f2dd2a49cbb0ba98d4085aa136e4e748fdeac4b4ab0ac353c1d856d581742477fd5c234cb335076452061e2ec47999af'
