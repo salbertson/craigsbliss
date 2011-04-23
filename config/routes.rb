d2451a1791049724985426fbaf38cd0325a77dba
Scott Albertson <scott.onix@gmail.com>
2011-04-22 22:58:12 -0700

    Made the profiles index action the root route.

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Craigsbliss::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "profiles#index"
 
   # See how all your routes lay out with "rake routes"
 
