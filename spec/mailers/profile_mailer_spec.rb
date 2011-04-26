8d3900dbcc7ee121a6495cce856e45bed893f337
Scott Albertson <scott.onix@gmail.com>
2011-04-26 08:09:47 -0700

    If the listing has an image then indicate that in the email summary.

--- spec/mailers/profile_mailer_spec.rb
+++ spec/mailers/profile_mailer_spec.rb
@@ -1,5 +1,15 @@
 require "spec_helper"
 
 describe ProfileMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "with picture listing" do
+    before(:each) do
+      @listings = [Listing.new(:title => "Free Firewood", :url => "http://google.com", :image => "image.jpg")]
+    end
+
+    it "should signify an image exists in the email" do
+      ProfileMailer.new_listing_email(@listings).deliver
+      mailer = ActionMailer::Base.deliveries.last
+      mailer.body.should include("Free Firewood (pic)")
+    end
+  end
 end
